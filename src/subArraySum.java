c4d894422768c9bfc4b6bbb536b92da0057229de
yaminikota <yamini.kota466@gmail.com>
2023-06-04 19:31:18 +0530

    Update README.md

--- src/subArraySum.java
+++ src/subArraySum.java
@@ -12,15 +12,41 @@ public class subArraySum {
     int[] arr = {3,4,6,7,8,9,2,5,10,13};
     int N = arr.length;
 
-    for(int start=0; start<N; start++){
-        for(int end = start; end<N; end++){
-            int sum = 0;
-            //iterate from start to end ,to get subarray
-            for(int i= start; i<=end; i++){
-                sum = sum + arr[i];
+        // TC: O(N^3) SC:O(N)
+        for(int start=0; start<N; start++){
+            for(int end = start; end<N; end++){
+                int sum = 0;
+                //iterate from start to end ,to get subarray
+                for(int i= start; i<=end; i++){
+                    sum = sum + arr[i];
+                }
+              //  System.out.println(sum);
             }
-            System.out.print(sum);
         }
+
+        prefixSumSubArray(arr);
+
     }
+
+    public static  void prefixSumSubArray(int[] arr){ // TC:O(N+N^2) SC:O(1)
+        int N = arr.length;
+        //creating & iterating over prefix sum Array
+        int[] psum = new int[N];
+        psum[0] = arr[0];
+        for(int i=1; i<N; i++){
+            psum[i] = psum[i-1] + arr[i];
+        }
+
+        for(int start=0; start<N; start++){
+            for(int end= start; end<N; end++){
+                int sum = 0;
+                if(sum == 0){
+                    sum = psum[end];
+                }else{
+                    sum = psum[end] - psum[start-1];
+                }
+                System.out.println(sum);
+            }
+        }
     }
 }
